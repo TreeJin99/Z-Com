c854420e3118f0c56a3c96417d50a098d43817b1
트리진 <treejin@teulijin-ui-MacBookPro.local>
2025-10-05 18:52:43 +0900

    Section 2 완료

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,11 @@
+// app/not-found.tsx
 import Link from "next/link";
-import {NextPage} from "next";
 
-const NotFound: NextPage = () => {
+export default function NotFound() {
     return (
         <div>
-            <div> 이 페이지는 존재하지 않습니다. 다른 페이지를 검색해 보세요. </div>
+            <div>이 페이지는 존재하지 않습니다. 다른 페이지를 검색해 보세요.</div>
             <Link href="/search">검색</Link>
         </div>
-    )
+    );
 }
-
-export default NotFound;
